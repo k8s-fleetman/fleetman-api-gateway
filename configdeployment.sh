745cd3d82d461a19a11f326b7664fa4ea4686c63
naveenkumarsp <naveenkumarsp@live.com>
2019-10-13 10:28:09 +0530

    Update configdeployment.sh

--- configdeployment.sh
+++ configdeployment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's+REPOSITORY_TAG+'"${REPOSITORY_TAG}"'+' deployment.yaml
-cat deployment.yaml | grep ${REPOSITORY_TAG}'
+sed -i 's+REPOSITORY_TAG+'"${REPOSITORY_TAG}"'+' deploy.yaml
+cat deploy.yaml | grep ${REPOSITORY_TAG}'
